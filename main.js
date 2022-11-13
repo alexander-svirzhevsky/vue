7d5b560866816c28844cb5cd500004652144467e
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2022-11-13 14:54:23 +0300

    wip

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ const app = Vue.createApp({
     return {
       title: "hello world",
       subTtile: "this is subTtile",
+      array: ["John", "Alex", "Henry"],
+      obj: {
+        key: "value Key 1",
+        key2: "value Key 2",
+      },
+      arrOfObj: [
+        { name: "Billy", sallary: 1000 },
+        { name: "Alex", sallary: 4000 },
+        { name: "Ivan", sallary: 200 },
+        { name: "Oleg", sallary: 100 },
+      ],
     };
   },
-  watch: {
-    title(newVal, prevVal) {
-      this.subTtile = this.reverseSubTitle();
-    },
-  },
   methods: {
-    handleClick(value) {
-      this.title === value;
-    },
     reverseSubTitle() {
       return this.subTtile.split("").reverse().join("");
+    },
+  },
+  computed: {
+    filteredBySallary() {
+      return this.arrOfObj.filter((item) => item.sallary >= 1000);
     },
   },
 });
