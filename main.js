1c0d676eb2f1a34e248be7f66a6779d4310159dd
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2022-11-14 15:22:40 +0300

    example with slots

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
-const app = Vue.createApp({
-  template: "#app",
+const container = {
+  template: "#container",
   data() {
     return {
-      title: "hello world",
-      subTtile: "this is subTtile",
-      array: ["John", "Alex", "Henry"],
-      obj: {
-        key: "value Key 1",
-        key2: "value Key 2",
-      },
-      arrOfObj: [
-        { name: "Billy", sallary: 1000 },
-        { name: "Alex", sallary: 4000 },
-        { name: "Ivan", sallary: 200 },
-        { name: "Oleg", sallary: 100 },
-      ],
+      list: ["one", "two", "three"],
     };
   },
-  methods: {
-    reverseSubTitle() {
-      return this.subTtile.split("").reverse().join("");
-    },
-  },
-  computed: {
-    filteredBySallary() {
-      return this.arrOfObj.filter((item) => item.sallary >= 1000);
-    },
+};
+
+const child = {
+  template: "#child",
+  props: ["title"],
+};
+
+const app = Vue.createApp({
+  template: "#app",
+  components: {
+    container,
+    child,
   },
 });
 
